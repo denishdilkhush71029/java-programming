8d3f71f82e2fde3426da4886798e3fbd7eda24dd
denishdilkhush71029 <dilkhushkumar71029@gmail.com>
2026-08-19 22:33:45 +0530

    Day 10

---
+++ java code/ReverseArray.java
@@ -0,0 +1,26 @@
+public class ReverseArray {
+    public static void reverseInPlace(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        reverseInPlace(arr);
+
+        // Output: 50 40 30 20 10
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
