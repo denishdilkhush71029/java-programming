16deecbcc80fd6b11dc7267c5caafc5b949eb48e
denishdilkhush71029 <dilkhushkumar71029@gmail.com>
2026-09-17 23:29:12 +0530

    Day 26

---
+++ java code/FactorialClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class FactorialClass{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of n: ");
+        int n=sc.nextInt();
+        int fact = 1;
+        int i=1;
+        for(i=1; i<=n; i++){
+            fact = fact*i;
+        }
+        System.out.println("The Factorial number n: " +fact);
+        sc.close();
+    }
+
+}
+class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
