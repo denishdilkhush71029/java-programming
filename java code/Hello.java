af1939430aeb08a365f4cd7f7df0b4ee92b1c3c3
denishdilkhush71029 <dilkhushkumar71029@gmail.com>
2026-09-20 23:35:00 +0530

    Day 29

--- java code/Hello.java
+++ java code/Hello.java
@@ -1,7 +1,14 @@
-class Hello
-{
-    public static void main (String args[])
-    {
-        System.out.println("Hello world ");
+import java.util.Scanner;
+public class Hello{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of N: ");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial Number of N: " +fact);
+        sc.close();
     }
 }
